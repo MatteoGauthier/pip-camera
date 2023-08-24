9419a632a8452a8f037bb61d2f5264d202dea265
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-08-24 16:56:00 +0200

    [ANALYTICS]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
 import "@/styles/globals.css"
 import "@/styles/ambient-player.css"
 
 import type { AppProps } from "next/app"
 
 import localFont from "next/font/local"
+import Script from "next/script"
 
 const CalSans = localFont({
   src: "../../public/fonts/CalSans-SemiBold.woff2",
@@ -14,6 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${CalSans.variable}`}>
       <Component {...pageProps} />
+      <noscript>
+        <img
+          alt="Shynet"
+          src="https://shynet-railway-production.up.railway.app/ingress/b8a1d84f-ddc0-4450-b677-0fffdff02070/pixel.gif"
+        />
+      </noscript>
+      <Script src="https://shynet-railway-production.up.railway.app/ingress/b8a1d84f-ddc0-4450-b677-0fffdff02070/script.js" />
     </main>
   )
 }
